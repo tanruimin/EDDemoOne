28487a1c2aa827f6010546c60ff606b6e641a33d
weizhen <939986850@qq.com>
2017-07-06 11:34:29 +0800

    我的改了添加地址

---
+++ app/src/main/java/threegroup/tl/com/eddemoone/mypage/activity/NewaddressActivity.java
@@ -0,0 +1,15 @@
+package threegroup.tl.com.eddemoone.mypage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import threegroup.tl.com.eddemoone.R;
+
+public class NewaddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newaddress);
+    }
+}
